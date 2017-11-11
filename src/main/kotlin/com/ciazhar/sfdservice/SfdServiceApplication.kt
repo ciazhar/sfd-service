bf15bf27e89d52f7c6e8f9e0867a4b86e72e1114
ciazhar <daisetsunakama@gmail.com>
2017-11-11 15:14:10 +0700

    initial commit

---
+++ src/main/kotlin/com/ciazhar/sfdservice/SfdServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.ciazhar.sfdservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SfdServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SfdServiceApplication>(*args)
+}
