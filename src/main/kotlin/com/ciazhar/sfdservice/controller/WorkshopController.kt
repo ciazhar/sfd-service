deda5b9379138a4ed77446e19b2c4c061fcb1228
ciazhar <daisetsunakama@gmail.com>
2017-11-11 17:01:12 +0700

    create register workshop api

---
+++ src/main/kotlin/com/ciazhar/sfdservice/controller/WorkshopController.kt
@@ -0,0 +1,26 @@
+package com.ciazhar.sfdservice.controller
+
+import com.ciazhar.sfdservice.model.mongo.User
+import com.ciazhar.sfdservice.service.UserService
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import javax.validation.Valid
+
+/**
+ * Created by ciazhar on 11/11/17.
+ *
+ * [ Documentation Here ]
+ */
+
+@RestController
+@RequestMapping("/workshop")
+class WorkshopController(private val service: UserService){
+
+    @PostMapping("/register")
+    fun register(@Valid @RequestBody user: User) : Mono<User>{
+        return service.registerWorkshop(user)
+    }
+}
