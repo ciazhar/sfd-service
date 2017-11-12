f44a661775eff21fbacd311835092e8eae3a4c93
ciazhar <ciazhar.id@gmail.com>
2017-11-12 15:54:23 +0700

    make find all stand api

--- src/main/kotlin/com/ciazhar/sfdservice/controller/StandController.kt
+++ src/main/kotlin/com/ciazhar/sfdservice/controller/StandController.kt
@@ -2,10 +2,8 @@ package com.ciazhar.sfdservice.controller
 
 import com.ciazhar.sfdservice.model.mongo.Stand
 import com.ciazhar.sfdservice.service.StandService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.Valid
 
@@ -22,5 +20,10 @@ class StandController(private val service : StandService){
     @PostMapping("/create")
     fun create(@Valid @RequestBody stand : Stand) : Mono<Stand>{
         return service.createStand(stand)
+    }
+
+    @GetMapping("/all")
+    fun findAll() : Flux<Stand>{
+        return service.findAll()
     }
 }
