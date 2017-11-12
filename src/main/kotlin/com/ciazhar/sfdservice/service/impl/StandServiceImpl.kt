8e1beefcf1da819a2dbe7a88b440b1d9a2b00763
ciazhar <daisetsunakama@gmail.com>
2017-11-12 10:50:48 +0700

    update stand service

--- src/main/kotlin/com/ciazhar/sfdservice/service/impl/StandServiceImpl.kt
+++ src/main/kotlin/com/ciazhar/sfdservice/service/impl/StandServiceImpl.kt
@@ -3,8 +3,10 @@ package com.ciazhar.sfdservice.service.impl
 import com.ciazhar.sfdservice.model.mongo.Stand
 import com.ciazhar.sfdservice.repository.StandRepository
 import com.ciazhar.sfdservice.service.StandService
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
 
+@Service
 class StandServiceImpl (private val standRepository: StandRepository) : StandService {
 
     override fun createStand(stand : Stand) : Mono<Stand>{
