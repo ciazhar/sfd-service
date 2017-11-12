06e969f6778ef547c7e47fd18dd58cdfc4185001
ciazhar <ciazhar.id@gmail.com>
2017-11-12 15:45:05 +0700

    make find all api

--- src/main/kotlin/com/ciazhar/sfdservice/controller/WorkshopController.kt
+++ src/main/kotlin/com/ciazhar/sfdservice/controller/WorkshopController.kt
@@ -4,10 +4,8 @@ import com.ciazhar.sfdservice.model.mongo.FestivalUser
 import com.ciazhar.sfdservice.model.mongo.WorkshopUser
 import com.ciazhar.sfdservice.service.FestivalUserService
 import com.ciazhar.sfdservice.service.WorkshopUserService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import javax.validation.Valid
 
@@ -24,5 +22,10 @@ class WorkshopController(private val service: WorkshopUserService){
     @PostMapping("/register")
     fun register(@Valid @RequestBody user: WorkshopUser) : Mono<WorkshopUser>{
         return service.registerWorkshop(user)
+    }
+
+    @GetMapping("/all")
+    fun findAll() : Flux<WorkshopUser>{
+        return service.findAll()
     }
 }
