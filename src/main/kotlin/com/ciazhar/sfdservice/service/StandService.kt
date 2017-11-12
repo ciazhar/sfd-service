270f7d152bce369693613114b8e2635244940392
ciazhar <daisetsunakama@gmail.com>
2017-11-12 10:34:24 +0700

    make create stand service

---
+++ src/main/kotlin/com/ciazhar/sfdservice/service/StandService.kt
@@ -0,0 +1,13 @@
+package com.ciazhar.sfdservice.service
+
+import com.ciazhar.sfdservice.model.mongo.Stand
+import reactor.core.publisher.Mono
+
+/**
+ * Created by ciazhar on 11/12/17.
+ *
+ * [ Documentation Here ]
+ */
+interface StandService {
+    fun createStand(stand : Stand) : Mono<Stand>
+}
---
+++ src/main/kotlin/com/ciazhar/sfdservice/service/impl/StandServiceImpl.kt
@@ -0,0 +1,13 @@
+package com.ciazhar.sfdservice.service.impl
+
+import com.ciazhar.sfdservice.model.mongo.Stand
+import com.ciazhar.sfdservice.repository.StandRepository
+import com.ciazhar.sfdservice.service.StandService
+import reactor.core.publisher.Mono
+
+class StandServiceImpl (private val standRepository: StandRepository) : StandService {
+
+    override fun createStand(stand : Stand) : Mono<Stand>{
+        return standRepository.save(stand)
+    }
+}
