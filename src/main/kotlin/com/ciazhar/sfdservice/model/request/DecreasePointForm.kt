a0daa73b797c1d35ac960dd71652f4f210c0a022
ciazhar <ciazhar.id@gmail.com>
2017-11-12 20:11:57 +0700

    make form for decrease point

---
+++ src/main/kotlin/com/ciazhar/sfdservice/model/request/DecreasePointForm.kt
@@ -0,0 +1,12 @@
+package com.ciazhar.sfdservice.model.request
+
+/**
+ * Created by ciazhar on 11/12/17.
+ *
+ * [ Documentation Here ]
+ */
+
+data class DecreasePointForm(
+    var participantId : String?=null,
+    var point : Int?=null
+)
